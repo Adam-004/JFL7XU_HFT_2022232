902605aacde0c6f13a5eb75b886756e862531f11
Balogh Adam <balogh.adam.00@stud.uni-obuda.hu>
2023-04-23 15:45:27 +0200

    Add IRepository interface

---
+++ JFL7XU_HFT_2022232.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JFL7XU_HFT_2022232.Repository.Interfaces
+{
+    interface IRepository<T> where T : class
+    {
+        //CRUD
+        void Create(T item);
+        IQueryable<T> ReadAll();
+        T Read(int id);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
