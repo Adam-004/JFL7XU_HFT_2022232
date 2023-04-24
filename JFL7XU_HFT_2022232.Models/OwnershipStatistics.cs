65438dfe7746f206a6c833e815d07a28e939c023
Balogh Adam <balogh.adam.00@stud.uni-obuda.hu>
2023-04-24 11:38:43 +0200

    Add OwnerShipStatistics class to Models

---
+++ JFL7XU_HFT_2022232.Models/OwnershipStatistics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JFL7XU_HFT_2022232.Models
+{
+    public class OwnershipStatistics
+    {
+        public OwnershipStatistics(Owner owner, Hangar hangar, IEnumerable<Starship> ships)
+        {
+            OwnerName = owner.Name;
+            OwnerAge = owner.Age;
+            HangarName = hangar.Name;
+            HangarLocation = hangar.Location;
+            Ships = ships;
+        }
+
+        string OwnerName;
+        int OwnerAge;
+        string HangarName;
+        string HangarLocation;
+        public IEnumerable<Starship> Ships { get; set; }
+
+        public override string ToString()
+        {
+            string Out = "";
+            Out += "Owner:\n" + OwnerName + " " + OwnerAge + " years old, has hangar at " + HangarLocation + ", named " + HangarName + ".\nOwners ships:\n";
+            int i=0;
+            foreach (var ship in Ships)
+            {
+                i++;
+                Out += "\n"+i +". ship: '"+ship.Name+"' type: "+((ShipType)ship.Type).ToString()+", built in "+ship.YearOfManu+".";
+            }
+            return Out;
+        }
+    }
+}
