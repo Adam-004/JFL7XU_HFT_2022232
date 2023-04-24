725b8663e1eeb842a9245c1d6b79d13e572879a9
Balogh Adam <balogh.adam.00@stud.uni-obuda.hu>
2023-04-24 11:06:31 +0200

    Finalize INonCrudLogic interface with 6 methods

--- JFL7XU_HFT_2022232.Logic/Interfaces/INonCrudLogic.cs
+++ JFL7XU_HFT_2022232.Logic/Interfaces/INonCrudLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JFL7XU_HFT_2022232.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace JFL7XU_HFT_2022232.Logic.Interfaces
 {
-    internal class INonCrudLogic
+    public interface INonCrudLogic
     {
+        IEnumerable<Starship> ListShips_WhichBuiltAfter(int year);
+        IEnumerable<Hangar> ListHangars_WithShipsMoreThan(int quantity);
+        IEnumerable<Hangar> ListHangars_WithShipsLessThan(int quantity);
+        IEnumerable<Owner> ListOwners_OlderThan(int year);
+        IEnumerable<Owner> ListOwners_YoungerThan(int year);
+        IEnumerable<Owner> ListOwners_YoungerAndHasMoreShipsThan(int year, int quantity);
     }
 }
