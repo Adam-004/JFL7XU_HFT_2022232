275239265973ae58fca20aeb41b7cc75c4f1cd4a
Balogh Adam <balogh.adam.00@stud.uni-obuda.hu>
2023-04-24 11:18:45 +0200

    Implement INonCrudLogic interface on NonCrudLogic

---
+++ JFL7XU_HFT_2022232.Logic/Logics/NonCrudLogic.cs
@@ -0,0 +1,25 @@
+﻿using JFL7XU_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JFL7XU_HFT_2022232.Logic.Logics
+{
+    public class NonCrudLogic
+    {
+        public IEnumerable<Starship> ListShips_WhichBuiltAfter(int year)
+        {
+
+        }
+        public IEnumerable<Starship> ListShips_WhichBuiltBefore(int year)
+        {
+
+        }
+        public IEnumerable<Hangar> ListHangars_WithShipsMoreThan(int quantity)
+        {
+            return repo.ReadAll().Where(h => h.Owner.Ships.Count > quantity);
+        }
+    }
+}
