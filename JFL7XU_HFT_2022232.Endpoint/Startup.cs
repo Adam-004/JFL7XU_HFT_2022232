7a9ca45258c4d9be327373bb5d3e8cf278274093
Balogh Adam <balogh.adam.00@stud.uni-obuda.hu>
2023-04-26 14:11:12 +0200

    Finalize Endpoint startup (Exception to Json)

--- JFL7XU_HFT_2022232.Endpoint/Startup.cs
+++ JFL7XU_HFT_2022232.Endpoint/Startup.cs
@@ -1,8 +1,16 @@
+using JFL7XU_HFT_2022232.Logic.Interfaces;
+using JFL7XU_HFT_2022232.Logic.Logics;
+using JFL7XU_HFT_2022232.Models;
+using JFL7XU_HFT_2022232.Repository.Database;
+using JFL7XU_HFT_2022232.Repository.Interfaces;
+using JFL7XU_HFT_2022232.Repository.Repos;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +24,23 @@ namespace JFL7XU_HFT_2022232.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<SpacecraftOwnershipDBContext>();
+
+            services.AddTransient<IRepository<Owner>, OwnerRepository>();
+            services.AddTransient<IRepository<Starship>, StarshipRepository>();
+            services.AddTransient<IRepository<Hangar>, HangarRepository>();
+
+            services.AddTransient<IOwnerLogic, OwnerLogic>();
+            services.AddTransient<IStarshipLogic, StarshipLogic>();
+            services.AddTransient<IHangarLogic, HangarLogic>();
+            services.AddTransient<INonCrudLogic, NonCrudLogic>();
+
+            services.AddControllers();
+            //services.AddSwaggerGen();
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { Title = "JFL7XU_HFT_2022232.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,7 +49,19 @@ namespace JFL7XU_HFT_2022232.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JFL7XU_HFT_2022232.Endpoint v1"));
             }
+
+            //For Exceptions to send in Json
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                .Get<IExceptionHandlerPathFeature>()
+                .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
