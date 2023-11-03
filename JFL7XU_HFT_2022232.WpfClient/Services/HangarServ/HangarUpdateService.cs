53aa3ef2f5e14f602273bf9c7ee63b0342103e3a
Adam-004 <balogh.adam.00@stud.uni-obuda.hu>
2023-11-03 16:02:54 +0100

    Remove possibility to edit Hangar ID (only display)

--- JFL7XU_HFT_2022232.WpfClient/Services/HangarServ/HangarUpdateService.cs
+++ JFL7XU_HFT_2022232.WpfClient/Services/HangarServ/HangarUpdateService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JFL7XU_HFT_2022232.Models;
+using JFL7XU_HFT_2022232.WpfClient.Services.OwnerServ;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace JFL7XU_HFT_2022232.WpfClient.Services.HangarServ
 {
-    internal class HangarUpdateService
+    class HangarUpdateService
     {
+        public HangarUpdateService() { }
+        public Hangar Update(Hangar hangar)
+        {
+            var updater = new HangarUpdateWindow(hangar);
+            if (updater.ShowDialog() == true)
+            {
+                hangar = updater.Updated;
+            }
+            return hangar;
+        }
     }
 }
--- JFL7XU_HFT_2022232.WpfClient/Services/HangarServ/HangarUpdateWindow.xaml.cs
+++ JFL7XU_HFT_2022232.WpfClient/Services/HangarServ/HangarUpdateWindow.xaml.cs
@@ -26,25 +26,14 @@ namespace JFL7XU_HFT_2022232.WpfClient.Services.HangarServ
             InitializeComponent();
             Updated = hangar;
             this.Title = $"Updating '{hangar.Name}'";
-            InputID.Text = Updated.Id.ToString();
+            ShowID.Content = Updated.Id.ToString();
             InputName.Text = Updated.Name.ToString();
             InputLocation.Text = Updated.Location.ToString();
             InputOwnerID.Text = Updated.OwnerID.ToString();
         }
         private void UpdateClick(object sender, RoutedEventArgs e)
         {
-            bool ParseOkId = false;
             bool ParseOkOID = false;
-            if (InputID.Text != "")
-            {
-                ParseOkId = int.TryParse(InputID.Text.ToString(), out int id);
-                if (ParseOkId)
-                {
-                    Updated.Id = id;
-                }
-                else MessageBox.Show("ID must be a number!");
-            }
-            else MessageBox.Show("ID was null!");
             if (InputName.Text != "") Updated.Name = InputName.Text.ToString(); else MessageBox.Show("Name was null!");
             if (InputLocation.Text != "") Updated.Location = InputLocation.Text.ToString(); else MessageBox.Show("Location was null!");
             if (InputOwnerID.Text != "")
@@ -52,12 +41,12 @@ namespace JFL7XU_HFT_2022232.WpfClient.Services.HangarServ
                 ParseOkOID = int.TryParse(InputOwnerID.Text.ToString(), out int id);
                 if (ParseOkOID)
                 {
-                    Updated.Id = id;
+                    Updated.OwnerID = id;
                 }
                 else MessageBox.Show("OwnerID must be a number!");
             }
             else MessageBox.Show("OwnerID was null!");
-            if (InputID.Text != "" && InputLocation.Text != "" && InputName.Text != "" && InputOwnerID.Text != "" && ParseOkId && ParseOkOID) this.DialogResult = true;
+            if (InputLocation.Text != "" && InputName.Text != "" && InputOwnerID.Text != "" && ParseOkOID) this.DialogResult = true;
         }
     }
 }
