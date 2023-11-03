f134b5a59207fd9da9250fec1ef1cb4fe71673fa
Adam-004 <balogh.adam.00@stud.uni-obuda.hu>
2023-11-03 09:32:51 +0100

    Create MainWindow View and ViewModel

---
+++ JFL7XU_HFT_2022232.WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JFL7XU_HFT_2022232.WpfClient.ViewModels
+{
+    partial class MainWindowViewModel
+    {
+        [RelayCommand]
+        public void EditOwners(Window window)
+        {
+            var OwnerWindow = new Window();
+            OwnerWindow.Show();
+            window.Close();
+        }
+        [RelayCommand]
+        public void Terminate(Window window) { window.Close(); }
+        public MainWindowViewModel() { }
+    }
+}
