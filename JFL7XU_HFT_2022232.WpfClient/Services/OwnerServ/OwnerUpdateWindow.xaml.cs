42edcd7c69c56ea413159821416cd147c2d53845
Adam-004 <balogh.adam.00@stud.uni-obuda.hu>
2023-11-05 16:50:58 +0100

    Remove function to edit ID on owner entities

--- JFL7XU_HFT_2022232.WpfClient/Services/OwnerServ/OwnerUpdateWindow.xaml.cs
+++ JFL7XU_HFT_2022232.WpfClient/Services/OwnerServ/OwnerUpdateWindow.xaml.cs
@@ -28,24 +28,13 @@ namespace JFL7XU_HFT_2022232.WpfClient.Services.OwnerServ
             Updated = owner;
             this.Title = $"Updating '{owner.Name}'";
             var id = Updated.ID.ToString();
-            InputID.Text = id;
+            ShowID.Content = id;
             InputName.Text = Updated.Name.ToString();
             InputAge.Text = Updated.Age.ToString();
         }
         private void UpdateClick(object sender, RoutedEventArgs e)
         {
-            bool ParseOkID = false;
             bool ParseOkAge = false;
-            if (InputID.Text != "")
-            {
-                ParseOkID = int.TryParse(InputID.Text.ToString(), out int id);
-                if (ParseOkID)
-                {
-                    Updated.ID = id;
-                }
-                else MessageBox.Show("ID must be a number!");
-            }
-            else MessageBox.Show("ID was null!");
             if (InputName.Text != "") Updated.Name = InputName.Text.ToString(); else MessageBox.Show("Name was null!");
             if (InputAge.Text != "")
             {
@@ -54,10 +43,10 @@ namespace JFL7XU_HFT_2022232.WpfClient.Services.OwnerServ
                 {
                     Updated.Age = age;
                 }
-                else MessageBox.Show("ID must be a number!");
+                else MessageBox.Show("Age must be a number!");
             }
-            else MessageBox.Show("ID was null!");
-            if (InputID.Text != "" && InputName.Text != "" && InputAge.Text != "" && ParseOkID && ParseOkAge) this.DialogResult = true;
+            else MessageBox.Show("Age was null!");
+            if (InputName.Text != "" && InputAge.Text != "" && ParseOkAge) this.DialogResult = true;
         }
     }
 }
