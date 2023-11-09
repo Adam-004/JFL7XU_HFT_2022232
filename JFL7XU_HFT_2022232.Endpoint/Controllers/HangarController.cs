1fa63131569a992e8bfbe95b7e7e253de24615b4
Adam-004 <balogh.adam.00@stud.uni-obuda.hu>
2023-11-09 15:39:23 +0100

    Add SignalRHub for controllers

--- JFL7XU_HFT_2022232.Endpoint/Controllers/HangarController.cs
+++ JFL7XU_HFT_2022232.Endpoint/Controllers/HangarController.cs
@@ -1,6 +1,9 @@
-﻿using JFL7XU_HFT_2022232.Logic.Interfaces;
+﻿using JFL7XU_HFT_2022232.Endpoint.Services;
+using JFL7XU_HFT_2022232.Logic.Interfaces;
 using JFL7XU_HFT_2022232.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,10 +15,11 @@ namespace JFL7XU_HFT_2022232.Endpoint.Controllers
     public class HangarController : ControllerBase
     {
         IHangarLogic logic;
-
-        public HangarController(IHangarLogic logic)
+        IHubContext<SignalRHub> hub;
+        public HangarController(IHangarLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         // GET: api/<OwnerController>
@@ -37,6 +41,7 @@ namespace JFL7XU_HFT_2022232.Endpoint.Controllers
         public void Create([FromBody] Hangar value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("HangarCreated", value);
         }
 
         // PUT api/<OwnerController>/5
@@ -44,13 +49,16 @@ namespace JFL7XU_HFT_2022232.Endpoint.Controllers
         public void Update([FromBody] Hangar value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("HangarUpdated", value);
         }
 
         // DELETE api/<OwnerController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var deleted = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("HangarDeleted", deleted);
         }
     }
 }
